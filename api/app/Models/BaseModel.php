9560dc110d4bf3b0b02cbe464bcaceb4b1470915
Johan Steen <artstorm@gmail.com>
2015-06-03 19:47:23 +0700

    Transforms updated_at and created_at to ISO 8601 strings

--- api/app/Models/BaseModel.php
+++ api/app/Models/BaseModel.php
@@ -1,9 +1,9 @@
 <?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-class BaseModel extends Model {
 
-
+class BaseModel extends Model
+{
     public $incrementing = false;
 
     public static function getTableName()
@@ -11,4 +11,25 @@ class BaseModel extends Model {
         return with(new static)->getTable();
     }
 
+    /**
+     * Accessor to get created_at as an ISO 8601 string.
+     *
+     * @param  string  $date
+     * @return string
+     */
+    public function getCreatedAtAttribute($date)
+    {
+        return \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $date)->toIso8601String();
+    }
+
+    /**
+     * Accessor to get updated_at as an ISO 8601 string.
+     *
+     * @param  string  $date
+     * @return string
+     */
+    public function getUpdatedAtAttribute($date)
+    {
+        return \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $date)->toIso8601String();
+    }
 }
