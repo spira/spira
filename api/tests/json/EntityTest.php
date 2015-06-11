aa156aea9c37ecd11b1706f26810b78d22348deb
Johan Steen <artstorm@gmail.com>
2015-06-11 17:04:54 +0700

    Example API test class.
    Example of a PHP unit class testing responses from routes. If opting to go
    this route, I'd suggest adding migration and seeding as part of the testing
    suite, and doing necessary adjustments on the vagrant box to allow for this.

---
+++ api/tests/json/EntityTest.php
@@ -0,0 +1,58 @@
+<?php
+
+class EntityTest extends TestCase
+{
+    use CrawlerTrait;
+
+    // Hard coded now, should be seeded during setUp() if going this route.
+    protected $first_entity_id = '04b26f6b-abdd-34b2-9721-adc72f184229';
+
+    // Note:
+    //
+    // If going this route, I'd think it's a good idea to migrate and seed
+    // the database as part of the test setUp().
+
+    /**
+     * Get all test entities.
+     */
+    public function testGetAll()
+    {
+        $this->get('/test/entities');
+
+        $this->assertResponseOk();
+        $this->shouldReturnJson();
+        $this->assertJsonArray();
+        $this->assertJsonMultipleEntries();
+    }
+
+    /**
+     * Get a test entity.
+     */
+    public function testGetOne()
+    {
+        $this->get('/test/entities/'.$this->first_entity_id);
+
+        $this->assertResponseOk();
+        $this->shouldReturnJson();
+
+        $object = json_decode($this->response->getContent());
+
+        $this->assertTrue(is_object($object), 'Response is an object');
+
+        $this->assertObjectHasAttribute('entityId', $object);
+        $this->assertStringMatchesFormat('%x-%x-%x-%x-%x', $object->entityId);
+        $this->assertTrue(strlen($object->entityId) === 36, 'UUID has 36 chars');
+
+        $this->assertTrue(is_string($object->varchar), 'Varchar column type is text');
+        $this->assertTrue(is_string($object->hash), 'Hash column is a hash');
+        $this->assertTrue(is_integer($object->integer), 'Integer column type is integer');
+        $this->assertTrue(is_float($object->decimal), 'Decimal column type is integer');
+        $this->assertNull($object->nullable, 'Nullable column type is null');
+        $this->assertTrue(is_string($object->text), 'Text column type is text');
+        $this->assertValidIso8601Date($object->date, 'Date column type is a valid ISO8601 date');
+        $this->assertObjectHasAttribute('multiWordColumnTitle', $object, 'Multi word colum is camel cased');
+
+        $this->assertValidIso8601Date($object->createdAt, 'createdAt column is a valid ISO8601 date');
+        $this->assertValidIso8601Date($object->updatedAt, 'updatedAt column is a valid ISO8601 date');
+    }
+}
