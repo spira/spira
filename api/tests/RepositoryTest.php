c0f94f20a6484a6dd7bfc26a213c3f76e1b9a674
Johan Steen <artstorm@gmail.com>
2015-06-17 12:00:43 +0700

    Adds initial tests for the Test Entity Repository

---
+++ api/tests/RepositoryTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use Mockery as m;
+
+class RepositoryTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->mock = m::mock('App\Models\TestEntity')->makePartial();
+        $this->app->instance('App\Models\TestEntity', $this->mock);
+
+        $this->repository = $this->app->make('App\Repositories\TestRepository');
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        m::close();
+    }
+
+    /**
+     * Test TestEntity Repository.
+     */
+    public function teastAll()
+    {
+        $this->mock->shouldReceive('get')
+            ->once()
+            ->with(m::type('array'))
+            ->andReturn([]);
+
+        $result = $this->repository->all();
+
+        $this->assertTrue(is_array($result));
+    }
+
+    public function tesatCreate()
+    {
+        $this->mock->shouldReceive('create')
+            ->once()
+            ->with(m::type('array'))
+            ->andReturn($this->mock);
+
+        $result = $this->repository->create(['foo' => 'bar']);
+
+        $this->assertTrue(is_object($result));
+    }
+
+    public function teastFind()
+    {
+        $this->mock->shouldReceive('findOrFail')
+            ->once()
+            ->andReturn($this->mock);
+
+        $result = $this->repository->find('foobar');
+
+        $this->assertTrue(is_object($result));
+    }
+
+    public function testFailingFind()
+    {
+        $this->app->instance('App\Models\TestEntity', null);
+        $repository = new App\Repositories\TestRepository($this->app);
+
+        $this->setExpectedException('Illuminate\Database\Eloquent\ModelNotFoundException');
+
+        $result = $repository->find(null);
+    }
+}
