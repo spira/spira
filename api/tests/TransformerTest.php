eb3ced7f08240f1765fe619bf2f7bae3c440f9bc
Johan Steen <artstorm@gmail.com>
2015-06-23 08:23:06 +0700

    Adds additional tests

--- api/tests/TransformerTest.php
+++ api/tests/TransformerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Mockery as m;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class TransformerTest extends TestCase
 {
@@ -24,5 +25,55 @@ class TransformerTest extends TestCase
             ->andReturn(['foo_bar' => 'foobar']);
 
         $this->assertArrayHasKey('fooBar', $this->service->item($data, new $this->transformer));
+    }
+
+    /**
+     * Testing Transformer Service.
+     */
+    public function testCollection()
+    {
+        $entities = factory(App\Models\TestEntity::class, 3)->make();
+
+        $collection = $this->service->collection($entities);
+
+        $this->assertCount(3, $collection);
+    }
+
+    public function testItem()
+    {
+        $entity = factory(App\Models\TestEntity::class)->make();
+
+        $item = $this->service->item($entity);
+
+        $this->assertTrue(is_array($item));
+    }
+
+    public function testNonArrayableItem()
+    {
+        $array = ['foo' => 'bar'];
+
+        $item = $this->service->item($array);
+
+        $this->assertTrue(is_array($item));
+    }
+
+    public function testPaginated()
+    {
+        $entities = factory(App\Models\TestEntity::class, 3)->make();
+
+        $paginated = new LengthAwarePaginator($entities, 3, 1, 1);
+
+        $collection = $this->service->paginatedCollection($paginated);
+
+        $this->assertTrue(is_array($collection));
+    }
+
+    public function testParseIncludes()
+    {
+        $entity = factory(App\Models\TestEntity::class)->make();
+
+        $this->service->parseIncludes('foobar');
+
+        $item = $this->service->item($entity);
     }
 }
