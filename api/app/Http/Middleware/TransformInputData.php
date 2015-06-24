63a7983833232fa03183e174bfb773667c572e09
Johan Steen <artstorm@gmail.com>
2015-06-24 14:44:09 +0700

    Implements input transformation in nested arrays

--- api/app/Http/Middleware/TransformInputData.php
+++ api/app/Http/Middleware/TransformInputData.php
@@ -15,7 +15,13 @@ class TransformInputData
     {
         foreach ($request->all() as $key => $value) {
 
-            // Find any potential camelCase keys, and convert them to snake_case
+            // Handle snakecase conversion in sub arrays
+            if (is_array($value)) {
+                $value = $this->renameKeys($value);
+            }
+
+            // Find any potential camelCase keys in the 'root' array, and convert
+            // them to snake_case
             if (!ctype_lower($key)) {
                 $request->offsetSet(snake_case($key), $value);
                 $request->offsetUnset($key);
@@ -24,5 +30,29 @@ class TransformInputData
         }
 
         return $next($request);
+    }
+
+    /**
+     * Recursively rename keys in nested arrays.
+     *
+     * @param  array  $array
+     * @return array
+     */
+    protected function renameKeys(array $array)
+    {
+        foreach($array as $key => $value) {
+
+            // Recursively check if the value is an array that needs parsing too
+            $value = (is_array($value)) ? $this->renameKeys($value) : $value;
+
+            // Convert camelCase to snake_case
+            if (!ctype_lower($key)) {
+                $array[snake_case($key)] = $value;
+                unset($array[$key]);
+            }
+
+        }
+
+        return $array;
     }
 }
--- api/tests/MiddlewareTest.php
+++ api/tests/MiddlewareTest.php
@@ -27,4 +27,29 @@ class MiddlewareTest extends TestCase
         $this->assertArrayNotHasKey('firstName', $request->all());
         $this->assertArrayHasKey('lastname', $request->all());
     }
+
+    public function testTransformInputDataNested()
+    {
+        $mw = new App\Http\Middleware\TransformInputData;
+
+        // Create a request object to test
+        $request = new Illuminate\Http\Request;
+        $request->offsetSet('firstName', 'foo');
+        $request->offsetSet('lastname', 'bar');
+        $request->offsetSet('nestedArray', ['fooBar' => 'bar', 'foo' => 'bar', 'oneMore' => ['andThis' => true]]);
+
+        // And a next closure
+        $next = function($request) { return $request; };
+
+        // Execute
+        $request = $mw->handle($request, $next);
+
+        // Assert
+        $this->assertArrayHasKey('first_name', $request->all());
+        $this->assertArrayNotHasKey('firstName', $request->all());
+        $this->assertArrayHasKey('lastname', $request->all());
+        $this->assertArrayHasKey('nested_array', $request->all());
+        $this->assertArrayHasKey('foo_bar', $request->nested_array);
+        $this->assertArrayHasKey('and_this', $request->nested_array['one_more']);
+    }
 }
