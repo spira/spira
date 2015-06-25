35555d02bb41476508fb958f5b9b9650540f4bc4
Johan Steen <artstorm@gmail.com>
2015-06-25 14:03:57 +0700

    Makes sure the id() method overwrites any existing set id in the data

--- api/app/Services/Validator.php
+++ api/app/Services/Validator.php
@@ -106,7 +106,7 @@ abstract class Validator
      */
     public function id($id)
     {
-        $this->data = array_add($this->data, $this->getKey(), $id);
+        $this->data[$this->getKey()] = $id;
 
         return $this;
     }
