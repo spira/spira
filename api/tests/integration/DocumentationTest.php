0557b9be05a7fdfe2d6a21b7961204222cdeadff
Zak Henry <zak.henry@gmail.com>
2015-06-26 22:52:05 +1000

    Added tests for documentation apib response, moved index test into the documentation test class

--- api/tests/integration/DocumentationTest.php
+++ api/tests/integration/DocumentationTest.php
@@ -6,7 +6,28 @@ use Illuminate\Support\Facades\Artisan;
 class DocumentationTest extends TestCase
 {
 
-    public function testDocumentation()
+    /**
+     * Index page (Apiary documentation) test
+     */
+    public function testIndexPage()
+    {
+        $this->get('/');
+
+        $this->assertResponseOk();
+
+        $this->see('<title>Spira - API Documentation</title>');
+    }
+
+    public function testDocumentationApib()
+    {
+        $this->get('/documentation.apib');
+
+        $this->assertResponseOk();
+        $this->see("FORMAT: 1A"); //see apiary format is present
+
+    }
+
+    public function testDocumentationIsValid()
     {
 
         $exitCode = Artisan::call('apiary:validate');
