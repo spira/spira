2048e5ab5f44746e0657cb42f71d8680f03d7fba
Johan Steen <artstorm@gmail.com>
2015-06-27 11:58:23 +0700

    Adds tests for failed batch validation

--- api/app/Services/Validator.php
+++ api/app/Services/Validator.php
@@ -190,7 +190,12 @@ abstract class Validator
 
         if (!empty(array_filter($errors))) {
 
-            throw new ValidationException(new MessageBag($errors));
+            // Use merge instead of passing the errors to the MessageBag
+            // constructor, to preserve nulls
+            $errorBag = new MessageBag();
+            $errorBag->merge($errors);
+
+            throw new ValidationException($errorBag);
         }
     }
 
