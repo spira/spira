a648c0073027ac4996ede0a1e3f31a29c7172848
Johan Steen <artstorm@gmail.com>
2015-07-01 13:50:06 +0700

    Implements support for transforming nested output data

--- api/tests/TransformerTest.php
+++ api/tests/TransformerTest.php
@@ -27,6 +27,33 @@ class TransformerTest extends TestCase
         $this->assertArrayHasKey('fooBar', $this->service->item($data, new $this->transformer));
     }
 
+    public function testItemNestedData()
+    {
+        $data = m::mock('Illuminate\Contracts\Support\Arrayable');
+        $data->shouldReceive('toArray')
+            ->once()
+            ->andReturn([
+                'foo_bar' => 'foobar',
+                'nested_data' => ['foo_bar' => true, 'foo' => true, 'bar_foo' => true]
+            ]);
+
+        $data = $this->service->item($data, new $this->transformer);
+
+        $this->assertArrayHasKey('fooBar', $data['nestedData']);
+        $this->assertArrayHasKey('barFoo', $data['nestedData']);
+        $this->assertArrayNotHasKey('foo_bar', $data['nestedData']);
+    }
+
+    public function testItemWithSelfKey()
+    {
+        $data = m::mock('Illuminate\Contracts\Support\Arrayable');
+        $data->shouldReceive('toArray')
+            ->once()
+            ->andReturn(['self' => 'foobar']);
+
+        $this->assertArrayHasKey('_self', $this->service->item($data, new $this->transformer));
+    }
+
     /**
      * Testing Transformer Service.
      */
