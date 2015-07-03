31ea30612ea48a4f2fbac4135ac357b4dfe8d2f2
Zak Henry <zak.henry@gmail.com>
2015-07-03 19:57:14 +1000

    Created alternative model factory syntax for better flexibility in documentation. Still untested

--- api/app/Services/ModelFactory.php
+++ api/app/Services/ModelFactory.php
@@ -3,10 +3,12 @@
 use App\Http\Transformers\BaseTransformer;
 use Illuminate\Container\Container;
 use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class ModelFactory
 {
 
+    protected $transformerService;
     protected $factory;
 
     /**
@@ -15,6 +17,26 @@ class ModelFactory
     public function __construct()
     {
         $this->factory = Container::getInstance()->make('Illuminate\Database\Eloquent\Factory');
+        $this->transformerService = App::make('App\Services\Transformer');
+    }
+
+    /**
+     * Get a factory instance
+     * @param $factoryClass
+     * @param $definedName
+     * @return $this
+     */
+    public function get($factoryClass, $definedName = false)
+    {
+
+        $factoryInstance = null;
+        if ($definedName){
+            $factoryInstance = $this->factory->of($factoryClass, $definedName);
+        }else{
+            $factoryInstance = $this->factory->of($factoryClass);
+        }
+
+        return new ModelFactoryInstance($factoryInstance, $this->transformerService);
     }
 
     /**
@@ -73,10 +95,148 @@ class ModelFactory
 
         }
 
-
         $jsonEncoded = json_encode($transformedEntity, JSON_PRETTY_PRINT);
 
         return str_replace("\n", "\n            ", $jsonEncoded); //cheap trick to make sure the 12 deep indentation requirement of apiary is preserved
     }
 
+}
+
+class ModelFactoryInstance
+{
+
+    private $transformerService;
+    private $factoryInstance;
+    private $customizations = [];
+    private $entityCount = 1;
+    private $transformer;
+    private $appends;
+    private $builtEntity;
+    private $makeVisible;
+    private $showOnly;
+
+
+    /**
+     * New model instance
+     * @param $factoryInstance
+     * @param $transformerService
+     */
+    public function __construct($factoryInstance, $transformerService)
+    {
+        $this->factoryInstance = $factoryInstance;
+        $this->transformerService = $transformerService;
+
+        return $this;
+    }
+
+
+    public function count($number)
+    {
+        $this->entityCount = $number;
+        return $this;
+    }
+
+    public function customize($customizations)
+    {
+        $this->customizations = $customizations;
+        return $this;
+    }
+
+    public function append($appends)
+    {
+        $this->appends = $appends;
+        return $this;
+    }
+
+    public function makeVisible($makeVisible)
+    {
+        $this->makeVisible = $makeVisible;
+        return $this;
+    }
+
+    public function showOnly($showOnly)
+    {
+        $this->showOnly = $showOnly;
+        return $this;
+    }
+
+    public function transform($transformerName)
+    {
+        $this->transformer = new $transformerName;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function build()
+    {
+
+        $entity = $this->factoryInstance
+            ->times($this->entityCount)
+            ->make($this->customizations)
+        ;
+
+        $entityType = ($this->entityCount > 1) ? 'collection' : 'item';
+
+        switch($entityType){
+            case 'item':
+                $entity = $this->modifyEntity($entity);
+                break;
+            case 'collection':
+                $entity = $entity->each(function($singleEntity){
+                    return $this->modifyEntity($singleEntity);
+                });
+                break;
+        }
+
+        if (!$this->transformer){
+            $this->transformer = new BaseTransformer();
+        }
+
+        $transformedEntity = $this->transformerService->{$entityType}($entity, $this->transformer);
+
+        $this->builtEntity = $transformedEntity;
+
+        return $this->builtEntity;
+    }
+
+
+    public function json()
+    {
+
+        if (!$this->builtEntity){
+            $this->build();
+        }
+
+        $entity = $this->builtEntity;
+
+        $jsonEncoded = json_encode($entity, JSON_PRETTY_PRINT);
+
+        return str_replace("\n", "\n            ", $jsonEncoded); //cheap trick to make sure the 12 deep indentation requirement of apiary is preserved
+
+    }
+
+    /**
+     * @param $entity
+     */
+    private function modifyEntity($entity)
+    {
+        if ($this->showOnly) {
+            $attributes = $entity->getAttributes();
+            $appends = $entity->appends;
+            $newHidden = array_diff(array_merge(array_keys($attributes), $appends), $this->showOnly);
+            $entity->setHidden($newHidden);
+        }
+
+        if ($this->makeVisible) {
+            $hidden = $entity->getHidden();
+
+            $newHidden = array_diff($hidden, $this->makeVisible);
+
+            $entity->setHidden($newHidden);
+        }
+
+        return $entity;
+    }
 }
