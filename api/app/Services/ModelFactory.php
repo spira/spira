5d34d6f460d4c190c3e788a604dd6c302db67dbb
Zak Henry <zak.henry@gmail.com>
2015-07-03 12:19:27 +1000

    Partial progress on defining the user models

--- api/app/Services/ModelFactory.php
+++ api/app/Services/ModelFactory.php
@@ -41,19 +41,41 @@ class ModelFactory
      * @param $factoryName
      * @param int $count
      * @param array $overrides
+     * @param array $properties white/blacklist of properties e.g. ['email'] will only show the email, ['password'] will hide the password if $blacklistProperties is true
+     * @param bool $blacklistProperties
      * @return string
      */
-    public function json($factoryName, $count = 1, $overrides = [])
+    public function json($factoryName, $count = 1, $overrides = [], $properties = [], $blacklistProperties = false)
     {
 
-        $entity = $this->make($factoryName, $count, $overrides);
-
+        $entity = $this->make($factoryName, $count, $overrides, $properties);
 
         $transformerService = App::make('App\Services\Transformer');
 
         $entityType = ($count > 1) ? 'collection' : 'item';
 
         $transformedEntity = $transformerService->{$entityType}($entity, new BaseTransformer);
+
+        if (!empty($properties)){
+            switch($entityType){
+                case 'collection':
+                {
+                    $transformedEntity = array_map(function($entity) use ($properties, $blacklistProperties){
+                        return $blacklistProperties ? array_except($entity, $properties) : array_only($entity, $properties);
+                    }, $transformedEntity);
+                }
+                    break;
+
+                case 'item':
+                default:
+                    $transformedEntity = $blacklistProperties ? array_except($entity, $properties) : array_only($entity, $properties);
+            }
+
+            dd($properties, $transformedEntity);
+        }
+
+
+
 
         return json_encode($transformedEntity, JSON_PRETTY_PRINT);
     }
