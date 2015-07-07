f1e14f4f0a3669b6c6344951421606d67c4279a9
Johan Steen <artstorm@gmail.com>
2015-07-07 10:29:30 +0700

    Pass through the previous exception object

--- api/app/Exceptions/UnprocessableEntityException.php
+++ api/app/Exceptions/UnprocessableEntityException.php
@@ -1,5 +1,6 @@
 <?php namespace App\Exceptions;
 
+use Exception;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class UnprocessableEntityException extends HttpException
@@ -8,10 +9,11 @@ class UnprocessableEntityException extends HttpException
      * Create a new Unprocessable Entity exception instance.
      *
      * @param  string  $message
+     * @param  \Exception $previous
      * @return void
      */
-    public function __construct($message = 'Unprocessable Entity.')
+    public function __construct($message = 'Unprocessable Entity.', Exception $previous = null)
     {
-        parent::__construct(422, $message);
+        parent::__construct(422, $message, $previous);
     }
 }
