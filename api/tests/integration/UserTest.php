51aff7deb4abbb9852c56227c5b604fb1e65c9fa
Johan Steen <artstorm@gmail.com>
2015-07-08 11:40:16 +0700

    Adds initial test suite for users integration test

---
+++ api/tests/integration/UserTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected function assertException($message, $statusCode, $exception)
+    {
+        $body = json_decode($this->response->getContent());
+        $this->assertResponseStatus($statusCode);
+        $this->assertContains($message, $body->message);
+        $this->assertContains($exception, $body->debug->exception);
+    }
+
+    public function testGetAllAdminUser()
+    {
+        $user = factory(App\Models\User::class)->make();
+        $user->user_type = 'admin';
+        $user->save();
+
+        $jwtAuth = $this->app->make('Tymon\JWTAuth\JWTAuth');
+        $token = $jwtAuth->fromUser($user);
+
+        $this->get('/users', [
+            'HTTP_AUTHORIZATION' => 'Bearer '.$token
+        ]);
+
+        $this->assertResponseOk();
+        $this->shouldReturnJson();
+        $this->assertJsonArray();
+        $this->assertJsonMultipleEntries();
+    }
+
+    public function testGetAllPublicUser()
+    {
+        $user = factory(App\Models\User::class)->make();
+        $user->user_type = 'public';
+        $user->save();
+
+        $jwtAuth = $this->app->make('Tymon\JWTAuth\JWTAuth');
+        $token = $jwtAuth->fromUser($user);
+
+        $this->get('/users', [
+            'HTTP_AUTHORIZATION' => 'Bearer '.$token
+        ]);
+
+        $this->assertException('Denied', 401, 'UnauthorizedException');
+    }
+}
