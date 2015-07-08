cd2ba96cad2a39670eeaefb38757bdb67f1acc90
Johan Steen <artstorm@gmail.com>
2015-07-08 10:57:17 +0700

    Initial implementation of Auth middleware

---
+++ api/app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,63 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Tymon\JWTAuth\JWTAuth;
+use Illuminate\Http\Request;
+use App\Exceptions\UnauthorizedException;
+
+class AuthMiddleware
+{
+    /**
+     * JWT Auth
+     *
+     * @var JWTAuth
+     */
+    protected $jwtAuth;
+
+    /**
+     * Assign dependencies.
+     *
+     * @param  JWTAuth  $jwtAuth
+     * @return void
+     */
+    public function __construct(JWTAuth $jwtAuth)
+    {
+        $this->jwtAuth = $jwtAuth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // @todo Consolidate this retrieve/verify token code with the controller refresh() method.
+        if (!$token = $this->jwtAuth->setRequest($request)->getToken()) {
+            throw new BadRequestException('Token not provided.');
+        }
+
+        try {
+            $user = $this->jwtAuth->authenticate((string) $token);
+        }
+        catch (TokenExpiredException $e) {
+            throw new UnauthorizedException('Token expired.', 401, $e);
+        }
+        catch (TokenInvalidException $e) {
+            throw new UnprocessableEntityException($e->getMessage(), 422, $e);
+        }
+
+        if (!$user) {
+            throw new RuntimeException('The user does not exist.');
+        }
+
+        if ($user->user_type !== 'admin') {
+            // @todo Replace with a 403 Forbidden Exception
+            throw new UnauthorizedException('Denied.', 401);
+        }
+
+        return $next($request);
+    }
+}
