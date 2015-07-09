0c9aa75c04664a60b75f1f0d88984027419cf55c
Johan Steen <artstorm@gmail.com>
2015-07-09 10:02:57 +0700

    Adds user validation rules

--- api/app/Http/Validators/UserValidator.php
+++ api/app/Http/Validators/UserValidator.php
@@ -5,6 +5,21 @@ use App\Services\Validator;
 class UserValidator extends Validator
 {
     /**
+     * Validation rules.
+     *
+     * @var array
+     */
+    protected $rules = [
+        'email' => 'required|string|email',
+        'first_name' => 'string',
+        'last_name' => 'string',
+        'phone' => 'string',
+        'mobile' => 'string',
+        'timezone_identifier' => 'timezone',
+        'user_type' => 'string|in:public,admin'
+    ];
+
+    /**
      * Model being validated.
      *
      * @var string
