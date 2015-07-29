9e6f4d896ddfb2542956761606dcb2418f441cbf
Johan Steen <artstorm@gmail.com>
2015-07-29 11:18:36 +0700

    Updates validation for the user claim

--- api/app/Extensions/JWTAuth/UserClaim.php
+++ api/app/Extensions/JWTAuth/UserClaim.php
@@ -1,8 +1,12 @@
-<?php namespace App\Extensions\JWTAuth;
+<?php
+
+namespace App\Extensions\JWTAuth;
 
 use App;
+use App\Models\User;
 use Tymon\JWTAuth\Claims\Claim;
 use App\Services\TransformerService;
+use Tymon\JWTAuth\Exceptions\InvalidClaimException;
 use App\Http\Transformers\IlluminateModelTransformer;
 
 class UserClaim extends Claim
@@ -17,17 +21,16 @@ class UserClaim extends Claim
     /**
      * Set the claim value, and call a validate method if available
      *
-     * @param $value
-     * @throws \Tymon\JWTAuth\Exceptions\InvalidClaimException
+     * @param  User  $value
+     * @throws InvalidClaimException
      * @return $this
      */
     public function setValue($value)
     {
-        if ($value) {
-            $transformerService = App::make(TransformerService::class);
-            $transformer = new IlluminateModelTransformer($transformerService);
-            $value = $transformer->transform($value);
-        }
+        // Transform the user before encoding
+        $transformerService = App::make(TransformerService::class);
+        $transformer = new IlluminateModelTransformer($transformerService);
+        $value = $transformer->transform($value);
 
         return parent::setValue($value);
     }
@@ -40,6 +43,6 @@ class UserClaim extends Claim
      */
     protected function validate($value)
     {
-        return is_array($value) || is_null($value);
+        return is_array($value);
     }
 }
