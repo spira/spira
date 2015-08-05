b3fd1210c61e995d6336bb48d89f7439db355dec
Johan Steen <artstorm@gmail.com>
2015-08-05 21:03:55 +0700

    Implements post install build script

---
+++ forum/src/Build.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Spira;
+
+use Composer\Script\Event;
+
+class Build
+{
+    /**
+     * Runs after a composer install.
+     *
+     * @param   Event   $event  [description]
+     * @return  void
+     */
+    public static function postInstall(Event $event)
+    {
+        self::getInstance()->buildVanillaApp();
+    }
+
+    /**
+     * Runs after a composer update.
+     *
+     * @param   Event   $event  [description]
+     * @return  void
+     */
+    public static function postUpdate(Event $event)
+    {
+        self::getInstance()->buildVanillaApp();
+    }
+
+    /**
+     * Combine the retrieved packages to the required application.
+     *
+     * @return void
+     */
+    protected function buildVanillaApp()
+    {
+        // Copy the main application to public
+        $this->recurseCopy('vendor/vanilla/vanilla', 'public');
+
+        // Copy the API module inside the application directory
+        $this->recurseCopy('vendor/kasperisager/vanilla-api', 'public/applications/api');
+    }
+
+    /**
+     * Recursively copy a directory structure.
+     *
+     * @param  string  $source
+     * @param  string  $dest
+     * @return void
+     */
+    protected function recurseCopy($source, $dest)
+    {
+        $dir = opendir($source);
+        @mkdir($dest);
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($source . '/' . $file)) {
+                    $this->recurseCopy($source . '/' . $file, $dest . '/' . $file);
+                } else {
+                    copy($source . '/' . $file, $dest . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+
+    /**
+     * Get an instance of the application builder.
+     *
+     * @return Build
+     */
+    protected static function getInstance()
+    {
+        return new Build;
+    }
+}
