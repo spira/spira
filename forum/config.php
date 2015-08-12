7f2af9fd8c0759db02e77b078491e4d993f50088
Zak Henry <zak.henry@gmail.com>
2015-08-12 10:04:48 +1000

    Added proxy to /forum for the vanilla forum, move the port to 8081 to be non-conflicting

--- forum/config.php
+++ forum/config.php
@@ -3,10 +3,10 @@
 }
 
 // Database
-$Configuration['Database']['Name'] = getenv('MYSQL_DATABASE');
+$Configuration['Database']['Name'] = getenv('VANILLADATABASE_ENV_MYSQL_DATABASE');
 $Configuration['Database']['Host'] = getenv('VANILLADATABASE_PORT_3306_TCP_ADDR');
-$Configuration['Database']['User'] = getenv('MYSQL_USER');
-$Configuration['Database']['Password'] = getenv('MYSQL_PASSWORD');
+$Configuration['Database']['User'] = getenv('VANILLADATABASE_ENV_MYSQL_USER');
+$Configuration['Database']['Password'] = getenv('VANILLADATABASE_ENV_MYSQL_PASSWORD');
 
 // EnabledApplications
 $Configuration['EnabledApplications']['Conversations'] = 'conversations';
