24682be2224bdf5c49a0bbe1a503c3101bfca8d4
Zak Henry <zak.henry@gmail.com>
2015-08-20 09:49:42 +1000

    Moved nested entities to the ApiController, implemented for ChildController

--- api/app/Http/Controllers/ApiController.php
+++ api/app/Http/Controllers/ApiController.php
@@ -8,7 +8,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\BadRequestException;
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller;
+use Spira\Model\Collection\Collection;
+use Spira\Model\Model\BaseModel;
 use Spira\Responder\Contract\TransformerInterface;
 use Spira\Responder\Response\ApiResponse;
 
@@ -43,4 +47,32 @@ abstract class ApiController extends Controller
     {
         return $this->transformer;
     }
+
+    /**
+     * @param Collection|BaseModel $modelOrCollection
+     * @param Request $request
+     * @return mixed
+     */
+    protected function getWithNested($modelOrCollection, Request $request)
+    {
+        if ((!$modelOrCollection instanceof BaseModel) && (!$modelOrCollection instanceof Collection)) {
+            throw new \InvalidArgumentException('Model must be instance of Model or Collection');
+        }
+
+        $nested = $request->headers->get('With-Nested');
+        if (!$nested) {
+            return $modelOrCollection;
+        }
+
+        $requestedRelations = explode(', ', $nested);
+
+        try {
+            $modelOrCollection->load($requestedRelations);
+        } catch (\BadMethodCallException $e) {
+            throw new BadRequestException(sprintf('Invalid `With-Nested` request - one or more of the following relationships do not exist for %s:[%s]', get_class($modelOrCollection), $nested), null, $e);
+        }
+
+        return $modelOrCollection;
+    }
+
 }
