74692a91f8a62ecf366efcbb7da57eef5a420b74
Jeremy Sik <jeremy.sik@me.com>
2015-08-20 16:54:59 +1000

    Adding skeleton unit test for listings controller.

---
+++ app/src/app/admin/articles/listing/listing.spec.ts
@@ -0,0 +1,45 @@
+namespace app.admin.articles.listing {
+
+    describe('Admin - Articles Listing', () => {
+
+        let articles:common.models.Article[],
+            $rootScope:global.IRootScope,
+            $scope:ng.IScope,
+            $stateParams:IArticlesListingStateParams = <IArticlesListingStateParams> {
+                page:1
+            },
+            articleService:common.services.article.ArticleService,
+            articlesPaginator:common.services.pagination.Paginator,
+            ArticlesListingController:ArticlesListingController;
+
+        beforeEach(() => {
+
+            module('app');
+
+            inject(($controller, _$rootScope_, _articleService_) => {
+                $rootScope = _$rootScope_;
+                $scope = $rootScope.$new();
+                articleService = _articleService_;
+
+                // Setup articlesPaginator before injection
+                articlesPaginator = articleService.getArticlesPaginator().setCount(10);
+
+                ArticlesListingController = $controller(app.admin.articles.listing.namespace + '.controller', {
+                    articlesPaginator: articlesPaginator,
+                    initArticles: articles,
+                    $stateParams: $stateParams
+                });
+            });
+
+        });
+
+        // Temporary until the controller is fleshed out more
+        it('should perform one test and pass it', () => {
+
+            expect(true).to.be.true;
+
+        });
+
+    });
+
+}
