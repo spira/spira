d146ffedfaba5dd4fc65c0f5af5c1011d007b41e
Johan Steen <artstorm@gmail.com>
2015-08-27 13:57:42 +0700

    Implements getting user id from JWT authed user

--- api/app/Extensions/Revisionable/ExtendsRevisionableTrait.php
+++ api/app/Extensions/Revisionable/ExtendsRevisionableTrait.php
@@ -2,9 +2,26 @@
 
 namespace App\Extensions\Revisionable;
 
+use App;
 use Venturecraft\Revisionable\RevisionableTrait;
 
 trait ExtendsRevisionableTrait
 {
     use RevisionableTrait;
+
+    /**
+     * Attempt to find the user id of the currently logged in user.
+     *
+     * @return string|null
+     */
+    private function getUserId()
+    {
+        $jwtAuth = App::make('Tymon\JWTAuth\JWTAuth');
+
+        if ($user = $jwtAuth->user()) {
+            return $user->user_id;
+        }
+
+        return null;
+    }
 }
