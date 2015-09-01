e053849152807e5d90a5e242fad5ef8b2fcb39f3
Zak Henry <zak.henry@gmail.com>
2015-09-01 13:12:50 +1000

    Extracted forum host into method, added fallback to FORUMSERVER_HOST for tests

--- api/app/Services/Api/Vanilla/Client.php
+++ api/app/Services/Api/Vanilla/Client.php
@@ -72,7 +72,7 @@ class Client
         $this->client = $client;
 
         $this->secret = env('VANILLA_API_SECRET');
-        $this->baseUrl = sprintf('http://%s:%s', Request::server('UPSTREAM_WEB_TCP_ADDR'), env('FORUMSERVER_PORT'));
+        $this->baseUrl = $this->getForumHost();
 
         $this->client->setBaseUrl($this->baseUrl);
         $this->client->getEventDispatcher()->addListener(
@@ -282,5 +282,17 @@ class Client
             'username' => $username,
             'email' => $email
         ];
+    }
+
+    /**
+     * Get the forum host url
+     * @return string
+     */
+    protected function getForumHost()
+    {
+        //try the forumserver first, then default to upstream (used by the runtime)
+        $host = strlen(env('FORUMSERVER_HOST') > 0) ? env('FORUMSERVER_HOST') : env('UPSTREAM_WEB_TCP_ADDR');
+
+        return sprintf('http://%s:%s', $host, env('FORUMSERVER_PORT'));
     }
 }
