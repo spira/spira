a9134f652031ef3e0995cd6804c14886aebb22ab
Zak Henry <zak.henry@gmail.com>
2015-09-04 18:20:17 +1000

    Applied StyleCi header patch

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Spira framework.
+ *
+ * @link https://github.com/spira/spira
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 Dotenv::load(__DIR__.'/../');
