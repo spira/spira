27a1b51f0720d0be95fe8287722fa0d7d3ed7bab
Zak Henry <zak.henry@gmail.com>
2015-09-04 15:30:28 +1000

    Fixed case where a dataset doesn't return an arry by hydrating an empty model with the array data so customizations can be applied.

---
+++ api/src/Model/Model/DataModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: redjik
+ * Date: 13.07.15
+ * Time: 19:24
+ */
+
+namespace Spira\Model\Model;
+
+class DataModel extends BaseModel
+{
+
+
+    protected static $unguarded = true;
+}
