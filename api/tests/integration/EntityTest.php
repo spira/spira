fa3cafc65e410cd397341a134b75ab0a57ad065a
Zak Henry <zak.henry@gmail.com>
2015-09-19 13:36:59 +1000

    Reverted invalid nested entity test that was failing due to https://github.com/laravel/framework/pull/10309

--- api/tests/integration/EntityTest.php
+++ api/tests/integration/EntityTest.php
@@ -305,8 +305,6 @@ class EntityTest extends TestCase
 
     public function testGetOneWithInvalidNested()
     {
-        $this->markTestSkipped("Skipped until https://github.com/laravel/framework/pull/10309 is fixed");
-
         $entity = factory(App\Models\TestEntity::class)->create();
         $this->addRelatedEntities($entity);
 
