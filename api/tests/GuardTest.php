1f1f7251e2f2be3a0bb4a9105e7bf2eac624a5a5
ivanmatveev <Redjiks@gmail.com>
2015-09-22 00:53:07 +0500

    coverage tests

--- api/tests/GuardTest.php
+++ api/tests/GuardTest.php
@@ -1,6 +1,32 @@
 <?php
 
+use Spira\Auth\Driver\Guard;
+use Spira\Contract\Exception\NotImplementedException;
+
 class GuardTest extends TestCase
 {
+    public function testNotImplemented()
+    {
+        $guard = $this->getGuard();
+        $this->setExpectedException(NotImplementedException::class, 'Not Implemented.');
+        $guard->basic();
 
+        $this->setExpectedException(NotImplementedException::class, 'Not Implemented.');
+        $guard->onceBasic();
+
+        $this->setExpectedException(NotImplementedException::class, 'Not Implemented.');
+        $guard->validate();
+
+        $this->setExpectedException(NotImplementedException::class, 'Not Implemented.');
+        $guard->viaRemember();
+
+    }
+
+    /**
+     * @return Guard
+     */
+    protected function getGuard()
+    {
+        return $this->app[Guard::class];
+    }
 }
