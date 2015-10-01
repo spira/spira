368b7d1508da280725c4221ca1633fdadd2ee6c8
ivanmatveev <Redjiks@gmail.com>
2015-10-01 22:58:32 +0500

    migrations return

--- api/src/Rbac/Access/UserNotFoundException.php
+++ api/src/Rbac/Access/UserNotFoundException.php
@@ -8,13 +8,6 @@
  * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
  */
 
-/**
- * Created by PhpStorm.
- * User: ivanmatveev
- * Date: 01.10.15
- * Time: 17:00.
- */
-
 namespace Spira\Rbac\Access;
 
 class UserNotFoundException extends \Exception
--- api/src/Rbac/Providers/RBACProvider.php
+++ api/src/Rbac/Providers/RBACProvider.php
@@ -8,13 +8,6 @@
  * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
  */
 
-/**
- * Created by PhpStorm.
- * User: ivanmatveev
- * Date: 24.09.15
- * Time: 19:19.
- */
-
 namespace Spira\Rbac\Providers;
 
 use Illuminate\Support\ServiceProvider;
