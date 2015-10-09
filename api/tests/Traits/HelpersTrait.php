c8af63fdc0d93b808d9e2ecc821a48481f7bcbef
ivanmatveev <Redjiks@gmail.com>
2015-10-09 17:49:07 +0500

    even more coverage

--- api/tests/Traits/HelpersTrait.php
+++ api/tests/Traits/HelpersTrait.php
@@ -94,7 +94,6 @@ trait HelpersTrait
         $auth = $this->app->make('auth');
         $payload = $auth->getPayloadFactory()->createFromUser($user);
         $payload = array_merge($payload, $customClaims);
-
         return $auth->getTokenizer()->encode($payload);
     }
 }
