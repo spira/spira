7cd725de21fae73e64721741a79881a65633e966
Zak Henry <zak.henry@gmail.com>
2015-11-04 16:47:36 +1100

    Fixed doc comment for localizable model interface.

--- api/src/Model/Model/LocalizableModelInterface.php
+++ api/src/Model/Model/LocalizableModelInterface.php
@@ -11,10 +11,9 @@
 namespace Spira\Model\Model;
 
 /**
- * A model needs to implement these methods to work in a relationship without
- * Eloquent trying to hit the database. To be used for models that doesn't
- * actually exist in the database but still needs to have a defined
- * relationship with a model that does exist in the database.
+ * Interface LocalizableModelInterface
+ * A model should implement this interface if it uses LocalizableModelTrait to get correct typehinting
+ * @package Spira\Model\Model
  */
 interface LocalizableModelInterface
 {
