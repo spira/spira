d7423ebc2029f4a3d8fdb769376f8489adfd5580
ivanmatveev <Redjiks@gmail.com>
2015-11-08 23:20:05 +0500

    style patch

--- api/app/Http/Middleware/AuthMiddleware.php
+++ api/app/Http/Middleware/AuthMiddleware.php
@@ -1,8 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Spira framework.
+ *
+ * @link https://github.com/spira/spira
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
 
 namespace App\Http\Middleware;
-
 
 use Closure;
 use Illuminate\Http\Request;
@@ -19,7 +25,7 @@ class AuthMiddleware
     public function handle(Request $request, Closure $next)
     {
         $user = $request->user();
-        if (!$user){
+        if (! $user) {
             throw new UnauthorizedException();
         }
 
