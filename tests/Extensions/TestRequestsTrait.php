d952a9e7c767a96880617884c348f15d6b08e01b
ivanmatveev <Redjiks@gmail.com>
2015-12-03 23:12:20 +0500

    cleaning

--- tests/Extensions/TestRequestsTrait.php
+++ tests/Extensions/TestRequestsTrait.php
@@ -8,11 +8,6 @@
  * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
  */
 
-use App\Models\User;
-use Faker\Factory as Faker;
-use Spira\Auth\Driver\Guard;
-use Spira\Rbac\Storage\StorageInterface;
-
 namespace Spira\Core\tests\Extensions;
 
 trait TestRequestsTrait
