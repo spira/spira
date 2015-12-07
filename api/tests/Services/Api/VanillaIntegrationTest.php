870f96244031adbd3ee86ab7f9fdf6ce3d90f263
ivanmatveev <Redjiks@gmail.com>
2015-12-07 18:00:57 +0500

    vanilla fix

--- api/tests/Services/Api/VanillaIntegrationTest.php
+++ api/tests/Services/Api/VanillaIntegrationTest.php
@@ -49,8 +49,6 @@ class VanillaIntegrationTest extends TestCase
         $this->client->setUser('no_user');
 
         $discussion = $this->client->api('discussions')->create('Foo', 'Bar', 1);
-        //if no exception, show discussion
-        var_dump($discussion);
     }
 
     /**
