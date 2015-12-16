57aae1a443bfe0ee08a5bb2ec9386a2b0352587d
ivanmatveev <Redjiks@gmail.com>
2015-12-16 16:44:20 +0500

    array merge recursive from yii2

---
+++ Helpers/Arr.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Spira\Core\Helpers;
+
+
+class Arr extends \Illuminate\Support\Arr
+{
+    /**
+     * Copypaste from yii2
+     *
+     * Merges two or more arrays into one recursively.
+     * If each array has an element with the same string key value, the latter
+     * will overwrite the former (different from array_merge_recursive).
+     * Recursive merging will be conducted if both arrays have an element of array
+     * type and are having the same key.
+     * For integer-keyed elements, the elements from the latter array will
+     * be appended to the former array.
+     * @param array $a array to be merged to
+     * @param array $b array to be merged from. You can specify additional
+     * arrays via third argument, fourth argument etc.
+     * @return array the merged array (the original arrays are not changed.)
+     */
+    public static function merge($a, $b)
+    {
+        $args = func_get_args();
+        $res = array_shift($args);
+        while (!empty($args)) {
+            $next = array_shift($args);
+            foreach ($next as $k => $v) {
+                if (is_int($k)) {
+                    if (isset($res[$k])) {
+                        $res[] = $v;
+                    } else {
+                        $res[$k] = $v;
+                    }
+                } elseif (is_array($v) && isset($res[$k]) && is_array($res[$k])) {
+                    $res[$k] = self::merge($res[$k], $v);
+                } else {
+                    $res[$k] = $v;
+                }
+            }
+        }
+        return $res;
+    }
+}
