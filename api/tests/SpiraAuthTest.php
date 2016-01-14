563bcba6db8e4f044774e9dbcb1cf2d054727d6d
Zak Henry <zak.henry@gmail.com>
2016-01-14 14:45:33 +1100

    Fixed commented out Request class use statement

--- api/tests/SpiraAuthTest.php
+++ api/tests/SpiraAuthTest.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
  */
 
-//use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Spira\Auth\Driver\Guard;
 
 class SpiraAuthTest extends TestCase
